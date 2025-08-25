bd08ae0cfe3732794f06fedc3343cbb996dba278
Sanjay b <sanjaysrivastavab@gmail.com>
2025-08-24 21:59:01 -0700

    bow_tfidf

--- test.py
+++ test.py
@@ -1,9 +1,34 @@
-class sol:
-    def bitwiseAnd(self, left: int, right: int) -> int:
-        res=left
-        for i in range(left, right + 1):
-            res &= i
-        print(res)
-s=sol()
-s.bitwiseAnd(1,2147483647)
-        
+# class sol:
+#     def bitwiseAnd(self, left: int, right: int) -> int:
+#         res=left
+#         for i in range(left, right + 1):
+#             res &= i
+#         print(res)
+# s=sol()
+# s.bitwiseAnd(1,2147483647)
+
+# a=[1,2,3]
+# b=[1,2,3]
+# print(a==b)
+
+# def valid()->bool:
+#     stack=[]
+#     dic = {"]":"[","}":"{",")":"("}
+# #     for c in s:
+# #         if c in dic:
+# #             if stack and stack[-1]==dic[c]:
+# #                 stack.pop()
+# #             else :
+# #                 return False
+# #         else :
+# #             stack.append(c)
+# #     print(True if not stack else False) 
+
+# # valid('()[]')
+#     for c in dic:
+#         print(dic[c])
+# valid()
+
+
+stack = []
+print(True if stack else False)
